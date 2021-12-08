decfd8ae80066ab09776be2135286ba0ba4932bb
Lilitha <ngelelilitha18@gmail.com>
2021-12-08 13:50:21 +0200

    add new files

---
+++ html-to-pdf/README.markdown
@@ -0,0 +1,5 @@
+# HTML to PDF
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/michelgefuni/pen/MJOJmm](https://codepen.io/michelgefuni/pen/MJOJmm).
+
+Simple code to export a specific area from page to PDF
--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-$('#exportForm').click(function(){
-  var pdf = new jsPDF('a', 'mm', 'a4');
+$("#exportForm").click(function () {
+  var pdf = new jsPDF("a", "mm", "a4");
   var firstPage;
-  
-  html2canvas($('#first-page'), {
-    onrendered: function(canvas) {
-      firstPage = canvas.toDataURL('image/jpeg', 1.0);
-    }
+
+  html2canvas($("#first-page"), {
+    onrendered: function (canvas) {
+      firstPage = canvas.toDataURL("image/jpeg", 1.0);
+    },
   });
-  
-  setTimeout(function(){
-    pdf.addImage(firstPage, 'JPEG', 5, 5, 200, 0);
-    
+
+
+
+  setTimeout(function () {
+    pdf.addImage(firstPage, "JPEG", 5, 5, 200, 0);
     pdf.save("export.pdf");
   }, 150);
 });
-
-
-// image
